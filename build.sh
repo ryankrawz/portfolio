1212b1a7efd41439759d3c2470197a5efe06cdc1
Ryan Krawczyk <rkrawczy@USMBP16rkrawczy.local>
2023-11-21 18:29:30 -0500

    Build 1700609370

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ng build --output-path docs --base-href /portfolio/browser/
+ng build --output-path docs --base-href /portfolio/
+# Angular separates distribution into browser and server folders.
+# In order to serve on GitHub pages, index.html and dependences must
+# be in root of docs folder.
+mv docs/browser/* docs
+rm -r docs/browser
+rm -r docs/server
 git add .
 git commit -m "Build $(date +"%s")"
 git push
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-TN4OEZI5.mjs";import"./chunk-ATW4OH4V.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
