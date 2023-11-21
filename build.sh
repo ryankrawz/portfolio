665ea5945d26e6316bee99016fab7698fb10cda6
Ryan Krawczyk <rkrawczy@USMBP16rkrawczy.local>
2023-11-21 18:54:05 -0500

    Build 1700610845

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ ng build --output-path docs --base-href /portfolio/
 mv docs/browser/* docs
 rm -r docs/browser
 rm -r docs/server
+# Override GitHub's 404 page with index.html so it routes to
+# the application's own 404 page.
+cp docs/index.html docs/404.html
+# Push commit to be built with GitHub Pages
 git add .
 git commit -m "Build $(date +"%s")"
 git push
