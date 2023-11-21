a0557b164b222d7f4686ab143a85b4a55dcb31f9
Ryan Krawczyk <rkrawczy@USMBP16rkrawczy.local>
2023-11-20 19:30:19 -0500

    Functional contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+import { LoadConfigService } from '../services/load-config.service';
+import { AppSections } from '../shared/enums/AppSections';
+
+interface Link {
+  icon: string;
+  url: SafeUrl;
+}
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +17,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  message: string = '';
+  links: Link[] | undefined;
+
+  constructor(
+    private loadConfigService: LoadConfigService,
+    private sanitizer: DomSanitizer,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadConfigService.getConfigForSection(AppSections.CONTACT).subscribe(contactObj => {
+      this.message = contactObj['message'];
+      const cleanLinks: Link[] = [];
+      for (let link of contactObj['links']) {
+        cleanLinks.push({
+          icon: link['icon'],
+          url: this.sanitizer.bypassSecurityTrustUrl(link['url']),
+        });
+      }
+      this.links = cleanLinks;
+    });
+  }
 
 }
